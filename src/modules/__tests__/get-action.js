f639ed03e2890b40f624a47fef2fd63b299023b0
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-02-15 00:44:32 -0300

    test: Add tests for `get-action`

---
+++ src/modules/__tests__/get-action.js
@@ -0,0 +1,31 @@
+const { makeGetAction } = require('../get-action');
+
+const createMock = (processArgv) => {
+  const deps = {
+    supportedActions: ['all', 'select'],
+    printer: { die: jest.fn() },
+    processArgv
+  };
+
+  return [makeGetAction(deps), deps];
+};
+
+describe('readdir', () => {
+  it('should return the third element of process.argv if a valid action if given', () => {
+    const [getAction, deps] = createMock([null, null, 'select']);
+    expect(getAction()).toBe('select');
+    expect(deps.printer.die).not.toHaveBeenCalled();
+  });
+
+  it('should die if an invalid action is provided', () => {
+    const [getAction, deps] = createMock([null, null, 'invalid']);
+    expect(getAction()).toBe(undefined);
+    expect(deps.printer.die).toHaveBeenCalledTimes(1);
+  });
+
+  it('should die if no action is provided', () => {
+    const [getAction, deps] = createMock([null, null]);
+    expect(getAction()).toBe(undefined);
+    expect(deps.printer.die).toHaveBeenCalledTimes(1);
+  });
+});
